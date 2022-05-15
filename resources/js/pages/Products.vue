e77c55459e5968a88f1fa1b833e8d53b39627da2
Ahmed Mohamed Ibrahim <32125808+Ahmed-Ibrahimm@users.noreply.github.com>
2022-05-16 01:28:35 +0200

    vue good table in products page

--- resources/js/pages/Products.vue
+++ resources/js/pages/Products.vue
@@ -35,6 +35,30 @@
                             </router-link>
                         </div>
                     </div>
+                    <vue-good-table
+                        :columns="columns"
+                        :rows="rows"
+                        :pagination-options="{
+                            enabled: true,
+                            perPage: 15,
+                            perPageDropdown: [3, 7, 9],
+                            nextLabel: 'next',
+                            prevLabel: 'prev',
+                        }"
+                        :search-options="{
+                            enabled: true,
+                        }"
+                        style="margin-top: 3%"
+                    >
+                        <template slot="table-row" slot-scope="props">
+                            <span v-if="props.column.field == 'option'">
+                                {{ props.row.name }}
+                            </span>
+                            <span v-else>
+                                {{ props.formattedRow[props.column.field] }}
+                            </span>
+                        </template>
+                    </vue-good-table>
                 </div>
             </div>
         </div>
@@ -42,12 +66,83 @@
 </template>
 <script>
 import Sidebar from "../components/Sidebar.vue";
+import { VueGoodTable } from "vue-good-table-next";
+import "vue-good-table-next/dist/vue-good-table-next.css";
 export default {
     components: {
         Sidebar,
+        VueGoodTable,
     },
     data() {
-        return {};
+        return {
+            columns: [
+                {
+                    label: "Product",
+                    field: "name",
+                },
+                {
+                    label: "Price",
+                    field: "age",
+                },
+                {
+                    label: "Category",
+                    field: "age",
+                    sortable: false,
+                },
+                {
+                    label: "Code",
+                    field: "Code",
+                },
+                {
+                    label: "Store",
+                    field: "Code",
+                    sortable: false,
+                },
+                {
+                    label: "Option",
+                    field: "option",
+                    sortable: false,
+                },
+            ],
+            rows: [
+                { id: 1, name: "John", age: 20, createdAt: "", score: 0.03343 },
+                {
+                    id: 2,
+                    name: "Jane",
+                    age: 24,
+                    createdAt: "2011-10-31",
+                    score: 0.03343,
+                },
+                {
+                    id: 3,
+                    name: "Susan",
+                    age: 16,
+                    createdAt: "2011-10-30",
+                    score: 0.03343,
+                },
+                {
+                    id: 4,
+                    name: "Chris",
+                    age: 55,
+                    createdAt: "2011-10-11",
+                    score: 0.03343,
+                },
+                {
+                    id: 5,
+                    name: "Dan",
+                    age: 40,
+                    createdAt: "2011-10-21",
+                    score: 0.03343,
+                },
+                {
+                    id: 6,
+                    name: "John",
+                    age: 20,
+                    createdAt: "2011-10-31",
+                    score: 0.03343,
+                },
+            ],
+        };
     },
 };
 </script>
