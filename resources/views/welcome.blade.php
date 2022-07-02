969a3468b86395ad51e94e465eed31743b9a405c
Ahmed Mohamed Ibrahim <32125808+Ahmed-Ibrahimm@users.noreply.github.com>
2022-07-02 14:46:45 +0200

    edit in pwa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
         href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;500;700&family=Poppins:wght@400;500;700&display=swap"
         rel="stylesheet" fetchpriority="low">
     <link rel="manifest" href="/manifest.json">
-
+    <meta name="theme-color" content="#369">
 </head>
 
 <body>
